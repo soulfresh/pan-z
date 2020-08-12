9725a38f39317000f485ed01bba03ebb1abbed73
Marc Wren <things@soul-fresh.com>
2020-08-12 11:13:47 -0600

    Upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const merge = require('webpack-merge');
+const merge = require('webpack-merge').merge;
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin');
 
 const parts = require('./webpack.parts');
